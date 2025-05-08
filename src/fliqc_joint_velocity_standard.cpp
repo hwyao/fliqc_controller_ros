7c518e5ac1e0a8f5e6cc4e8ccf05ac52480b237f
Haowen Yao <haowen.yao@tum.de>
2025-05-08 09:16:49 +0200

    add force update call to diagnostic updater in stopping method for APFJointVelocity and FLIQCJointVelocityStandard

--- src/fliqc_joint_velocity_standard.cpp
+++ src/fliqc_joint_velocity_standard.cpp
@@ -663,6 +663,7 @@ void FLIQCJointVelocityStandard::stopping(const ros::Time& /*time*/) {
   // WARNING: DO NOT SEND ZERO VELOCITIES HERE AS IN CASE OF ABORTING DURING MOTION
   // A JUMP TO ZERO WILL BE COMMANDED PUTTING HIGH LOADS ON THE ROBOT. LET THE DEFAULT
   // BUILT-IN STOPPING BEHAVIOR SLOW DOWN THE ROBOT.
+  diag_updater_->force_update();
 }
 
 }  // namespace fliqc_controller_ros
